2ba955fa6d2f7c0a07ce51c723a41ccad014e8e8
HeavyWeather <delta.thrives@gmail.com>
2014-10-31 15:20:07 +0000

    Create nCDF_basics.py

---
+++ nCDF_basics.py
@@ -0,0 +1,8 @@
+def nCDF_basics():
+  """ acts as a single module import hub for basic netCDF handling tools used as:
+  
+  from nCDF_basics import *
+  
+  """
+  
+  import check_cdo, check_nco, nco_concatenate, nco_extract, nco_hyperslab
